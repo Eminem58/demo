eff0fbadb86b2e62412052bb4626055728f96346
jinbiao <459840682@qq.com>
2019-06-09 12:31:15 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/dao/impl/gitDaoImpl.java
@@ -0,0 +1,17 @@
+package com.example.demo.dao.impl;
+
+import com.example.demo.dao.gitDao;
+
+/**
+ * 〈〉
+ *
+ * @author jinbiao
+ * @create 2019.06.09
+ */
+public class gitDaoImpl implements gitDao {
+
+    @Override
+    public void add() {
+        System.out.println("hello");
+    }
+}

c9ca2a45eb03e19d0fe33ad89e75cd8452664738
金彪 <459840682@qq.com>
2019-06-15 19:17:46 +0800

    重建项目

--- src/main/java/com/example/demo/transaction/ExtTransactionUtil.java
+++ src/main/java/com/example/demo/transaction/ExtTransactionUtil.java
@@ -1,7 +1,12 @@
 package com.example.demo.transaction;
 
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.interceptor.DefaultTransactionAttribute;
 
 /**
  * 〈手写事务〉
@@ -14,8 +19,37 @@ import org.springframework.stereotype.Component;
 public class ExtTransactionUtil {
 
 
-    public void begin(){
+    @Autowired
+    private DataSourceTransactionManager dataSourceTransactionManager;
+
+
+
+    @Around("execution(* com.example.demo..*.*(..))")
+    public void around(){
+
 
     }
 
+
+
+
+
+
+
+
+
+
+
+    public TransactionStatus begin() {
+        TransactionStatus transactionStatus=dataSourceTransactionManager.getTransaction(new DefaultTransactionAttribute());
+        return transactionStatus;
+    }
+
+    public void commit(TransactionStatus transactionStatus){
+        dataSourceTransactionManager.commit(transactionStatus);
+    }
+
+    public void rollback(TransactionStatus transactionStatus){
+        dataSourceTransactionManager.rollback(transactionStatus);
+    }
 }

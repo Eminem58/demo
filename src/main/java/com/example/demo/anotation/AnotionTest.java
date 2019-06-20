ae6b777a4bc376f6a1e577aacf41c2ce15306e2d
jinbiao <459840682@qq.com>
2019-06-20 23:11:16 +0800

    重建项目

---
+++ src/main/java/com/example/demo/anotation/AnotionTest.java
@@ -0,0 +1,21 @@
+package com.example.demo.anotation;
+
+import java.lang.reflect.Method;
+
+/**
+ * 〈〉
+ *
+ * @author jinbiao
+ * @create 2019/6/18
+ * @since 1.0.0
+ */
+public class AnotionTest {
+    public static void main(String[] args) throws ClassNotFoundException {
+        Class<?> clz = Class.forName("com.example.demo.anotation.User");
+        Method[] methods = clz.getDeclaredMethods();
+        for (Method m : methods) {
+            MyAnnotation annotation = m.getDeclaredAnnotation(MyAnnotation.class);
+            System.out.println(annotation.toString());
+        }
+    }
+}

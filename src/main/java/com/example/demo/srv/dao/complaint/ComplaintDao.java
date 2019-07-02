e173cd6fbf059cfcbde35df2d2831e1af573098d
金彪 <459840682@qq.com>
2019-07-02 20:26:31 +0800

    重建项目

--- src/main/java/com/example/demo/srv/dao/complaint/ComplaintDao.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.srv.dao.complaint;
-
-import com.example.demo.srv.bo.ComplaintBO;
-
-public interface ComplaintDao {
-    ComplaintBO addComplaint(ComplaintBO complaintBO);
-}
